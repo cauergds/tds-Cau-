98cf35654e3fb6d3f5c1e0d31595b939d548fa8d
cauergds <112014407+cauergds@users.noreply.github.com>
2022-08-24 15:24:16 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Oi, tudo bem?")
+print("uma imagem de um cachorro...")
+print("0____")
+print(" ||||")
+print('''meu esporte favorito é futebol:
+         0/
+        /|
+      o / \    ''')
+print('14'*5)
+print('bra' + 'sil')
+
